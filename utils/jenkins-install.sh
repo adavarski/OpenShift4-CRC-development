28c0276cb162d7fda3f871fafe27ca83e4fb8297
A.Davarski <42774466+adavarski@users.noreply.github.com>
2021-06-21 18:09:42 +0300

    Create jenkins-install.sh

---
+++ utils/jenkins-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install jenkins
+sudo apt update
+sudo apt install -y openjdk-11-jre-headless
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
